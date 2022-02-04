cc2bb2d0484ad078f554d2eec68b231208ddeb35
issam-abdou <issamabdou001@gmail.com>
2022-02-04 17:28:31 +0100

    build the app

---
+++ build/precache-manifest.9778c833eb4f79306305a1b076a24dc2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67cfb98f2fd1a3021404db887b540cd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f71b5cdda19bd7ce8b76",
+    "url": "/static/css/main.336c4042.chunk.css"
+  },
+  {
+    "revision": "5414d2579694aea401d2",
+    "url": "/static/js/2.50b8907d.chunk.js"
+  },
+  {
+    "revision": "51fe97e89231493f9eac67ba37259b61",
+    "url": "/static/js/2.50b8907d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f71b5cdda19bd7ce8b76",
+    "url": "/static/js/main.93da5ad9.chunk.js"
+  },
+  {
+    "revision": "7e3d5fb5dc5d826e9d90",
+    "url": "/static/js/runtime-main.6a8aaf0a.js"
+  },
+  {
+    "revision": "d0c54d11ea3841298f0c0cecfb6a0271",
+    "url": "/static/media/arrow.d0c54d11.svg"
+  },
+  {
+    "revision": "4dd5809e3ddc5436ef5daccb5355648a",
+    "url": "/static/media/cart.4dd5809e.svg"
+  },
+  {
+    "revision": "176425bf0b53c249d9d495d02ff57126",
+    "url": "/static/media/hero.176425bf.png"
+  },
+  {
+    "revision": "fa1e58b79d668a9a579c0987f2a3d03c",
+    "url": "/static/media/item-1.fa1e58b7.png"
+  },
+  {
+    "revision": "1a17e4b5008ee876a4217c38c3ba37f5",
+    "url": "/static/media/item-2.1a17e4b5.png"
+  },
+  {
+    "revision": "90ed35252447e6f24909c14a68d9f93b",
+    "url": "/static/media/item-3.90ed3525.png"
+  },
+  {
+    "revision": "4f7d42dfa1c7cadc132b3970b88f8f82",
+    "url": "/static/media/item-4.4f7d42df.png"
+  },
+  {
+    "revision": "1b42bb5e132c6063f6eb2013948d986a",
+    "url": "/static/media/item-5.1b42bb5e.png"
+  },
+  {
+    "revision": "ecde975e03be2a9b21aab48d919ee735",
+    "url": "/static/media/item-6.ecde975e.png"
+  },
+  {
+    "revision": "dce114dec10fe97fa709424671473f2e",
+    "url": "/static/media/item-7.dce114de.png"
+  },
+  {
+    "revision": "cc8474d12ddc8e052f81fc6b317667ab",
+    "url": "/static/media/item-8.cc8474d1.png"
+  },
+  {
+    "revision": "5d61c534594b2e2ba3c3d6c560cbcb15",
+    "url": "/static/media/item-9.5d61c534.png"
+  },
+  {
+    "revision": "0f27d0b1be39f848d4b97b06644b519e",
+    "url": "/static/media/logo.0f27d0b1.svg"
+  },
+  {
+    "revision": "6a97656714be3fad78c7b4a7167b2c46",
+    "url": "/static/media/mailing-list-img.6a976567.png"
+  }
+]);
